d76333e31706e28c351149dc076cfa0c88124cd9
Andea Balducci <mtb.snowboard@gmail.com>
2016-04-08 20:50:43 +0200

    Initial commit

---
+++ Kernel/Actors/CommandWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Akka.Actor;
+
+namespace Kernel
+{
+	public class CommandWorker : UntypedActor
+	{
+		public CommandWorker ()
+		{
+			Console.WriteLine ("Command worker created");
+		}
+
+		#region implemented abstract members of UntypedActor
+
+		protected override void OnReceive (object message)
+		{
+		}
+
+		#endregion
+	}
+}
+
---
+++ Kernel/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Routing;
+
+namespace Kernel
+{
+	public static class Runner
+	{
+		static ActorSystem _system;
+
+		static Runner ()
+		{
+		}
+
+
+		public static void Start(){
+			_system = ActorSystem.Create ("kernel");
+
+			var props = Props.Create<CommandWorker>().WithRouter(new RoundRobinPool(10));
+			var worker = _system.ActorOf(props, "worker");
+		}
+			
+		public static void Stop(){
+			_system.ActorSelection ("akka://kernel/user").Tell (PoisonPill.Instance);
+			_system.WhenTerminated.Wait();
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Runner.Start ();
+
+			Console.WriteLine ("KERNEL Started, press any key to stop");
+			Console.ReadLine ();
+			Runner.Stop ();
+			Console.WriteLine ("KERNEL Stopped");
+		}
+	}
+}
